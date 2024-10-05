84d5d630b2672157ec2a51762f294d2af605bc4f
Arewebs <arewebs@gmail.com>
2024-10-04 23:42:48 -0400

    Add readme

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
+    return redirect()->route('cars.index');
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
